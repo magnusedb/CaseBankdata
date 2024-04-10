db9e0cf12ee70a7f31f8d6869bbaa03e5587f170
magnusedb <115003394+magnusedb@users.noreply.github.com>
2024-04-10 20:53:17 +0200

    ,

---
+++ CaseBankdata.tests/AccountServiceTest.cs
@@ -0,0 +1,24 @@
+namespace CaseBankdata.tests;
+using Xunit;
+using Moq;
+
+public class AccountServiceTest
+{
+    [Fact]
+    public void CreateAccountValidID()
+    {
+
+    }
+
+    [Fact]
+    public void CreateAccountValidDeposit()
+    {
+
+    }
+
+    [Fact]
+    public void CreateAccountInvalidID()
+    {
+
+    }
+}
